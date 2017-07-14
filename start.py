c4a6261415a0b98946670de14ba9ea09cb4196aa
estevesguilherme <guilhermeesteves@outlook.com.br>
2017-07-13 23:06:46 -0300

    Cria classe goChatTransa

--- start.py
+++ start.py
@@ -1,33 +1,22 @@
-from chatterbot.trainers import ListTrainer
-from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot import ChatBot
-from chatterbot.response_selection import get_random_response
-from chatterbot.storage import storage_adapter
+import sys
+from gochatbot import GoChatBot
 import logging
-bot = ChatBot(
-    "Go Transa",
-    storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
-    database='gotransa',
-    database_uri='mongodb://192.241.245.34:27017/',
-    logic_adapters=[
-        "chatterbot.logic.MathematicalEvaluation",
-        "chatterbot.logic.TimeLogicAdapter",
-        "chatterbot.logic.BestMatch"
-    ],
-    input_adapter="chatterbot.input.TerminalAdapter",
-    output_adapter="chatterbot.output.TerminalAdapter",
-    database_name="C:/Users/Lucas/estudos/chatter bot/database.sqlite3"
-)
 
-print("Type something to begin...")
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-# The following loop will execute each time the user enters input
-while True:
-    try:
-        # We pass None to this method because the parameter
-        # is not used by the TerminalAdapter
-        bot_input = bot.get_response(None)
+goChatBot = GoChatBot()
 
-    # Press ctrl-c or ctrl-d on the keyboard to exit
-    except (KeyboardInterrupt, EOFError, SystemExit):
-        break
+print("Gotransa is begin..")
+print(goChatBot.GetAnswer("oi"))
+
+# # The following loop will execute each time the user enters input
+# while True:
+#     try:
+#         # We pass None to this method because the parameter
+#         # is not used by the TerminalAdapter
+#         bot_input = bot.get_response(None)
+
+#     # Press ctrl-c or ctrl-d on the keyboard to exit
+#     except (KeyboardInterrupt, EOFError, SystemExit):
+#         break
